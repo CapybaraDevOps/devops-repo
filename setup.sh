4c29848f6f9b693376d07ab17441e238e06bea3c
Borys <wushatneen@gmail.com>
2024-04-24 15:15:47 +0300

    added rkhunter audit to setup.sh output is located in /var/log/rkhunter.log after machines creation

--- setup.sh
+++ setup.sh
@@ -15,8 +15,14 @@ END
 chmod 600 ~/.ssh/config
 
 # Ensure if sftp is installed and started 
-sudo apt update
+sudo apt -y update
 sudo apt install openssh-server -y
 sudo systemctl start ssh
 sudo systemctl enable ssh
+
+# Ensure if rkhunter is installed
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y vsftpd rkhunter
+sudo rkhunter --update
+sudo rkhunter --propupd
+sudo rkhunter -c --enable all --disable none --skip-keypress
 
