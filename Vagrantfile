44f7f2a57706e38c240f7cc611ac4a7842abf994
Misha <muhaluk322@gmail.com>
2024-04-25 21:49:07 +0300

    Inventory Automation & SFTP Group setup

--- Vagrantfile
+++ Vagrantfile
@@ -1,14 +1,14 @@
 
-########################Variables########################
-NODE_COUNT = 2					# Amount of VMs
+######################## Variables ########################
+NODE_COUNT = 2							# Amount of VMs
 BOX_IMAGE = "ubuntu/jammy64"			# Ubuntu 22.04
-VM_NAME = "node"				# VM name
-IP = "192.168.100."				# Private subnet
+VM_NAME = "node"						# VM name
+IP = "192.168.100."						# Private subnet
 KEY_PATH = "./ssh_keys/vagrant_rsa"		# PATH to SSH key
-KEY_PATH_DEST = "~/.ssh/vagrant_rsa"		# Destination to SSH PATH
-RAM = "2048"					# RAM
-CPUS = 1					# CPU
-########################Variables########################
+KEY_PATH_DEST = "~/.ssh/vagrant_rsa"	# Destination to SSH PATH
+RAM = "2048"							# RAM
+CPUS = 1								# CPU
+######################## Variables ########################
 
 Vagrant.configure("2") do |config|
 	# Templete for VMs, count configured in var NODE_COUNT
@@ -36,7 +36,7 @@ Vagrant.configure("2") do |config|
 			vb.vm.provision "file", source: KEY_PATH, destination: KEY_PATH_DEST
 			vb.vm.provision "file", source: "#{KEY_PATH}.pub", destination: "#{KEY_PATH_DEST}.pub"
 			# run script
-			vb.vm.provision "shell", path: "setup.sh", privileged: false
+			vb.vm.provision "shell", path: "setup.sh", privileged: false, args: [i, NODE_COUNT, IP, VM_NAME]
 		end
 	end
 end
--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@
 cat ~/.ssh/vagrant_rsa.pub >> ~/.ssh/authorized_keys
 # Set read pervition for privat key
 chmod 400 ~/.ssh/vagrant_rsa
-
 # Create SSH config file
 cat <<END > ~/.ssh/config
 Host 192.168.100.*
@@ -13,10 +12,28 @@ Host 192.168.100.*
     IdentitiesOnly yes
 END
 chmod 600 ~/.ssh/config
-echo "PermitRootLogin no" | sudo tee -a /etc/ssh/sshd_config 
-
 # Ensure if sftp is installed and started 
 sudo apt -y update
+sudo apt install openssh-server -y
+sudo systemctl start ssh
+sudo systemctl enable ssh
+# Create Group sftp and add user vagrant
+sudo groupadd sftp
+sudo usermod -aG sftp vagrant
+# Restrict root login and add configuration for sftp group
+cat <<EOF | sudo tee -a /etc/ssh/sshd_config > /dev/null
+PermitRootLogin no
+Match Group sftp
+    PasswordAuthentication no
+    #ChrootDirectory %h
+    PermitTunnel no
+    AllowAgentForwarding no
+    AllowTcpForwarding no
+    X11Forwarding no
+EOF
+sudo systemctl restart ssh
+# Ensure if sftp is installed and started 
+sudo apt update -y
 sudo apt install openssh-server -y
 sudo systemctl start ssh
 sudo systemctl enable ssh
@@ -31,3 +48,16 @@ sudo rkhunter --propupd 1>/dev/null #apply configuration
 # Run rkhunter once, in background, the output is in /var/log/rkhunter.log
 sudo rkhunter -c --enable all --disable none --skip-keypress --rwo 1>/dev/null &
 rm /tmp/vagrant-shell #self destruction of vagrant shell file since it considered as suspicious by rkhunter
+
+# Create invenroy file with IP of other VMs
+NODE_INDEX=$1
+NODE_COUNT=$2
+IP_HOST=$3
+VM_NAME=$4
+for j in $(seq 1 $NODE_COUNT); do
+	if [ $j -ne $NODE_INDEX ]; then
+		node_ip="${IP_HOST}$((9 + j))"
+		node_name="${VM_NAME}${j}"
+		echo "$node_name $node_ip" | sudo tee -a ~/inventory
+	fi
+done
